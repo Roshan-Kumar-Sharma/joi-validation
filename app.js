8be48ab09cce853e47df6bd2c38f9067e25c9c61
roshan <roshankumarsharmamika@gmail.com>
2021-09-01 12:37:25 +0530

    added few more complex joi validation

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-const express = require('express')
-require('dotenv').config()
+const express = require("express");
+require("dotenv").config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-const appRouter = require('./routes/app.routes')
+const appRouter = require("./routes/app.routes");
 
 const app = express();
 
-require('./configs/app.configs')(app)
+require("./configs/app.configs")(app);
 
-
-app.use(appRouter)
-
+app.use(appRouter);
 
 app.listen(PORT, () => {
-    console.log(`server is up at port ${PORT}`)
-})
+    console.log(`server is up at port ${PORT}`);
+});
--- configs/validation.js
+++ configs/validation.js
@@ -1,19 +1,45 @@
-const joi = require('joi')
+const joi = require("joi");
 
 const userSchema = joi.object({
-    username: joi.string().alphanum().min(3).required(),
+    username: joi
+        .string()
+        .pattern(
+            new RegExp("^(?!_)(?:[A-Za-z0-9]+|([_])(?!\1))*(?!_)([A-Za-z0-9])$")
+        )
+        .messages({
+            "string.pattern.base":
+                "Username must not contain underscore at start and end and mutiple underscores in a row is not allowed",
+        })
+        .min(3)
+        .trim()
+        .required(),
+    bio: joi.string(),
     email: joi.string().email().lowercase().required(),
-    password: joi.string().required().pattern(new RegExp('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[-+_!@#$%^&*.,?])')),
-    birthday: joi.date().iso()
-})
+    password: joi
+        .string()
+        .pattern(
+            new RegExp(
+                "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[-+_!@#$%^&*.,?])"
+            )
+        )
+        .trim()
+        .min(4)
+        .max(8)
+        .messages({
+            "string.pattern.base":
+                "Password must be atleast 4 characters long, with a mix of uppercase, lowercase, number and special characters",
+        })
+        .required(),
+    birthday: joi.date().iso(),
+});
 
 const postSchema = joi.object({
     htmlContent: joi.string().required(),
     tags: joi.array().items(joi.string().required()).max(5).required(),
     reactions: joi.object({
-        clapped: joi.array()
+        clapped: joi.array(),
     }),
-    postedBy: joi.string().required()
-})
+    postedBy: joi.string().required(),
+});
 
-module.exports = {userSchema, postSchema}
+module.exports = { userSchema, postSchema };
